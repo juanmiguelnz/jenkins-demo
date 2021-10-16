d84852de548c4ff7bf84c00297567c78d2a9d0ee
miguel <miguelkarlojuan@gmail.com>
2021-10-17 01:21:21 +1300

    test

--- sample-script.sh
+++ sample-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "${env.AZ_USER}"
-# az login --service-principal -u ${AZ_USER} -p ${AZ_PASSWORD} --tenant ${AZ_TENANT}
+
+az login --service-principal -u "${AZ_USER}" -p "${AZ_PASSWORD}" --tenant "${AZ_TENANT}"
