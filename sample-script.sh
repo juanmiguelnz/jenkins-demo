4c25812f7d9c251aed6e428db5f2ae7228d3159b
miguel <miguelkarlojuan@gmail.com>
2021-10-17 01:59:29 +1300

    clean up

--- sample-script.sh
+++ sample-script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# echo "$AZ_USER"
-# echo $AZ_PASSWORD
-az login --service-principal -u "$AZ_USER" -p "$AZ_PASSWORD" --tenant 6dc416ce-c43d-4371-b1cc-186d73f88d5e
+az login --service-principal -u $AZ_USER -p $AZ_PASSWORD --tenant AZ_TENANT
